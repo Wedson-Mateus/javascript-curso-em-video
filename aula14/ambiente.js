391e484c669017465be20eaa962e9830d628305d
Wedson Mateus <wedson.matheus0@gmail.com>
2022-09-20 08:08:39 -0300

    Conteúdo sobre laços

---
+++ aula14/ambiente.js
@@ -0,0 +1,15 @@
+/*
+var c = 1
+while (c <= 10) {
+    console.log(c)
+    c += 1
+}
+*/
+
+//---------------------
+
+console.log('Vai comecar...')
+for (var c = 1; c <= 10; c++) {
+    console.log(c)
+}
+console.log('FIM!')
